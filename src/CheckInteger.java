9007bf39ae1bfe730541a84e1f11ee553cb58029
trancuong3 <161544695+trancuong3@users.noreply.github.com>
2024-08-23 07:18:18 +0700

    Update CheckInteger.java

--- src/CheckInteger.java
+++ src/CheckInteger.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CheckInteger {
+import java.util.Scanner;
+
+public class CheckInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter some input: ");
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            System.out.println("You entered an integer: " + number);
+        } else {
+            System.out.println("Not an integer.");
+        }
+    }
 }
