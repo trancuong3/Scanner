2e667a104d34e1f767d3fe0e470c31aa80492be8
trancuong3 <161544695+trancuong3@users.noreply.github.com>
2024-08-23 07:18:30 +0700

    Update CheckString.java

--- src/CheckString.java
+++ src/CheckString.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CheckString {
+import java.util.Scanner;
+
+public class CheckString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter some input: ");
+        if (scanner.hasNext()) {
+            String input = scanner.next();
+            System.out.println("You entered a string: " + input);
+        } else {
+            System.out.println("Not a valid string.");
+        }
+    }
 }
